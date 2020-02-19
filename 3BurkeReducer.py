625532755b9af4282beee1718a1b4af9e30810f4
[Stephen Burke] <[burke.stephenpaul@gmail.com]>
2020-02-19 11:02:21 -0600

    [added my sort and shuffle and reducer]

---
+++ 3BurkeReducer.py
@@ -0,0 +1,30 @@
+s = open("ptss_2.txt","r")
+r = open("ptss_3.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+
+for line in s:
+  data = line.strip().split('\t')
+  pitch_type, start_speed = data
+
+  if pitch_type != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(str(thisKey) + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = pitch_type
+    thisValue = 0.0
+
+  # apply the aggregation function
+  #thisValue += float(amount)
+  #thisValue += float (start_speed)
+  if(thisValue < float(start_speed)):
+    thisValue = float(start_speed)
+
+# output the final entry when done
+r.write(str(thisKey) + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
