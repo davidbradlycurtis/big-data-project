7de048813e3e7f21ca6a6a25a8f8735898e598b0
Stephen Burke <burke.stephenpaul@gmail.com>
2020-02-19 13:19:11 -0600

    [copied my files into my folder]

---
+++ burke/1Mapper.py
@@ -0,0 +1,14 @@
+#Get/Create data files
+input_data = open("../data/pitches.csv", "r")
+output_data = open("ptss_1.txt", "w")
+
+#Iterates through the data, mapping key pitch speed (end_speed), to the value play result (type)
+for line in input_data:
+    datalist = line.strip().split(",")
+    px,pz,start_speed,end_speed,spin_rate,spin_dir,break_angle,break_length,break_y,ax,ay,az,sz_bot,sz_top,type_confidence,vx0,vy0,vz0,x,x0,y,y0,z0,pfx_x,pfx_z,nasty,zone,code,type,pitch_type,event_num,b_score,ab_id,b_count,s_count,outs,pitch_num,on_1b,on_2b,on_3b = datalist
+    if(pitch_type == '' or start_speed == ''): continue
+    output_data.write(pitch_type + "\t" + start_speed + "\n")
+
+input_data.close()
+output_data.close()
+print("Mapper Complete")
