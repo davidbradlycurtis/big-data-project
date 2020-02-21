e195ed8cfd18c50d4c9707c9b086f1da705ac088
David Curtis <46725522+davidbradlycurtis@users.noreply.github.com>
2020-02-21 14:06:37 -0600

    Update Comments for SortShuffle

--- curtis/2SortShuffle.py
+++ curtis/2SortShuffle.py
@@ -1,5 +1,5 @@
-unsorted = open("1.txt", "r")
-sorted = open("2.txt", "w")
+unsorted = open("1.txt", "r") #Opens mapped data
+sorted = open("2.txt", "w") #Writes sorted mapped data to 2.txt
 
 dataList = unsorted.readlines()
 unsorted.close()
